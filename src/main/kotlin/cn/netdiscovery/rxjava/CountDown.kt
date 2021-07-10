d4d3330b7f9a57e4c1d75b88a2168c2fd135b084
fengzhizi715 <fengzhizi715@126.com>
2021-07-10 22:58:45 +0800

    init the project

---
+++ src/main/kotlin/cn/netdiscovery/rxjava/CountDown.kt
@@ -0,0 +1,30 @@
+package cn.netdiscovery.rxjava
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
+
+/**
+ *
+ * @FileName:
+ *          cn.netdiscovery.rxjava.CountDown
+ * @author: Tony Shen
+ * @date: 2021-07-10 21:46
+ * @version: V1.0 <描述当前版本功能>
+ */
+fun countDown(
+    delay: Long,
+    unit: TimeUnit,
+    scheduler: Scheduler = Schedulers.computation(),
+    func: () -> Unit,
+    failure: ((Throwable) -> Unit)? = null
+): Disposable {
+    return Observable.timer(delay, unit, scheduler)
+        .subscribe({
+            func.invoke()
+        }, {
+            failure?.invoke(it)
+        })
+}
