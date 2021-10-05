aa9aba5675e2359e60306a23cc7072d8e5c6d176
Tony Shen <fengzhizi715@126.com>
2021-10-05 11:25:05 +0800

    增加注释

--- src/main/kotlin/cn/netdiscovery/rxjava/Delay.kt
+++ src/main/kotlin/cn/netdiscovery/rxjava/Delay.kt
@@ -12,7 +12,15 @@ import java.util.concurrent.TimeUnit
  *          cn.netdiscovery.rxjava.Delay
  * @author: Tony Shen
  * @date: 2021-07-19 00:24
- * @version: V1.0 <描述当前版本功能>
+ * @version: V1.0 延时相关的函数
+ */
+
+/**
+ * 延时函数
+ * @param delay     从什么时候开始
+ * @param unit      时间的单位
+ * @param scheduler 线程调度器（默认使用 computation 线程，当然也可以使用 io 等线程）
+ * @param action    延时的 block
  */
 fun delay(
     delay: Long,
@@ -24,6 +32,14 @@ fun delay(
         action.invoke()
     }
 
+/**
+ * 延时函数
+ * @param delay              从什么时候开始
+ * @param unit               时间的单位
+ * @param subscribeScheduler 线程调度器（默认使用 computation 线程，当然也可以使用 io 等线程）
+ * @param observeScheduler   下游的线程调度器，如果下游需要切换线程可以使用
+ * @param action             延时的 block
+ */
 fun delay(
     delay: Long,
     unit: TimeUnit,
