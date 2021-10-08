b4742d28cc4a44641e0478069b2533390b606de0
Tony Shen <fengzhizi715@126.com>
2021-10-08 12:10:26 +0800

    Update AutoRefresh.kt

--- utils/src/main/kotlin/cn/netdiscovery/rxjava/AutoRefresh.kt
+++ utils/src/main/kotlin/cn/netdiscovery/rxjava/AutoRefresh.kt
@@ -15,13 +15,23 @@ import java.util.concurrent.TimeUnit
  * @version: V1.0 自动刷新相关的函数
  */
 
+/**
+ * 自动刷新的函数
+ * @param initialDelay 从什么时候开始
+ * @param period       每隔一定的时间，执行刷新的功能
+ * @param unit         时间的单位
+ * @param scheduler    线程调度器（默认使用 computation 线程，当然也可以使用 io 等线程）
+ * @param func         刷新的 block
+ * @param failure      刷新失败的 block
+ */
 fun refresh(
+    initialDelay: Long,
     period: Long,
     timeUnit: TimeUnit,
     scheduler: Scheduler = Schedulers.computation(),
     func: () -> Unit,
     failure: ((Throwable) -> Unit)? = null
-): Disposable = Observable.interval(0, period, timeUnit, scheduler)
+): Disposable = Observable.interval(initialDelay,period, timeUnit, scheduler)
     .subscribe({
         func.invoke()
     }, {
